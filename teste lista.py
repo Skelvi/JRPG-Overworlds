9743157b5d935d600ae8f0f1384719fbf780256d
Skelvi <kelvinbfeliz@gmail.com>
2023-07-22 20:16:06 -0300

    Delete teste lista.py

--- teste lista.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import random
-
-respostas=np.genfromtxt("lista texto.txt",dtype="str",delimiter=",")
-numquestao=random.randint(0,len(respostas)-1)
-questao = respostas[numquestao]
-imagemnome = questao[0]
-print(imagemnome)
